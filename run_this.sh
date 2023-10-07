56e038f42959b27770a46f6fbbd62f0273c466a6
samsepi0lw <147035842+samsepi0lw@users.noreply.github.com>
2023-10-07 14:12:22 +0000

    Update run_this.sh

--- run_this.sh
+++ run_this.sh
@@ -10,7 +10,7 @@ git config --global user.name samsepi0lw
 git config --global user.email samsepi0lw@proton.me
 git config --global github.user samsepi0lw
 git config --global github.token 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
-git remote set-url origin
+git remote set-url origin git@github.com:samsepi0lw/ransom.git
 git add ./data.txt
 git commit -m "Uploaded data file"
 git push origin master
